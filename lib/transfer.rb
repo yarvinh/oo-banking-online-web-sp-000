272ac0da7fabb4d4a92e85cde98f70e9e96324c0
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-29 20:07:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver , :amount, :status
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  def valid?
+    if @sender.valid? == true && @receiver.valid? == true
+      true
+    else false
+    end
+  end
+  def execute_transaction
+    if valid? == true
+      if @sender.balance < 1
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      else
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    end
+    end
+  end
+
 end
